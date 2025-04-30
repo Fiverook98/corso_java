c840933265b6b64df70a8431e1c0eb9638d3d333
Fra <162883721+Fiverook98@users.noreply.github.com>
2025-04-30 18:03:04 +0200

    Delete TestProgramma/Test directory

--- TestProgramma/Test/Logico1/src/test.java
+++
@@ -1,9 +0,0 @@
-public class test{
-    public static void main(String args[]){
-        int a = 10, b = 20;
-        boolean d;
-        
-        d = !(a + b > 5);//NOT TRUE 
-        System.out.printf("Il valore di d è %b\n", d);
-    }
-}
